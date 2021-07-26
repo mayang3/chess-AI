f598f108c402dddcb359f11e6694b438ff5652a3
neo82 <junbeom.bae@worksmobile.com>
2021-07-26 13:48:31 +0900

    minimax chess commit

---
+++ src/main/java/com/neo82/chess/ChessApplication.java
@@ -0,0 +1,29 @@
+package com.neo82.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@RestController
+public class ChessApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessApplication.class, args);
+	}
+
+	@GetMapping("/chess")
+	public ModelAndView chess() {
+		return new ModelAndView("chess.html");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/img/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+	}
+}
